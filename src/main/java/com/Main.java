62ae18b28bcd09f02db8d30d8d5732455c5758cc
Bamo0507 <bryanalbertomo@gmail.com>
2024-05-08 07:31:06 -0600

    Re-commit de base de datos

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,9 +2,16 @@ package com;
 
 import org.neo4j.driver.*;
 import org.neo4j.driver.Record;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        //Nombre de archivo
+        String csvFile = "movies.csv";
+        String line;
         Driver driver = GraphDatabase.driver(
                 "neo4j+s://9c5d5c00.databases.neo4j.io", //"bolt://localhost:7687",
                 AuthTokens.basic(
@@ -13,86 +20,143 @@ public class Main {
 
                         ));
 
-        try (Session session = driver.session()) {
+        //Creación de Nodos
+        try(BufferedReader br = new BufferedReader(new FileReader(csvFile))){
+            line = br.readLine(); //Se salta la línea de encabezados
+            //Manejo para no repetir nodos
+            ArrayList<String> nodosAgregados = new ArrayList<>();
+            //Leer el archivo completo
+            while((line = br.readLine()) != null){
+                //Validación de nodos creados
+                boolean peliculaEncontrada = false;
+                boolean actorEncontrado = false;
+                boolean generoEncontrado = false;
+                boolean directorEncontrado = false;
 
+                //Dividir todo en secciones
+                String[] data = line.split(",");
+
+                //Obtener nombre de la película, y validar si se agrega
+                String pelicula = data[0];
+                for(String nodo:nodosAgregados){
+                    if(pelicula.equals(nodo)){
+                        peliculaEncontrada = true;
+                    }
+                } if(!peliculaEncontrada){
+                    crearNodosPelicula(driver, pelicula);
+                    nodosAgregados.add(pelicula);
+                }
+
+                //Obtener nombre Director, y validar si se agrega
+                String director = data[1];
+                for(String nodo:nodosAgregados){
+                    if(director.equals(nodo)){
+                        directorEncontrado = true;
+                    }
+                } if(!directorEncontrado){
+                    crearNodosDirector(driver, director);
+                    nodosAgregados.add(director);
+                }
+
+                //Obtener actor principal, y validar si se agrega
+                String actor = data[2];
+                for(String nodo:nodosAgregados){
+                    if(actor.equals(nodo)){
+                        actorEncontrado = true;
+                    }
+                } if(!actorEncontrado){
+                    crearNodosActor(driver, actor);
+                    nodosAgregados.add(actor);
+                }
+
+                //Obtener Género, y validar si se agrega
+                String genero = data[3];
+                for(String nodo: nodosAgregados){
+                    if(genero.equals(nodo)){
+                        generoEncontrado = true;
+                    }
+                } if(!generoEncontrado){
+                    crearNodosGenero(driver, genero);
+                    nodosAgregados.add(genero);
+                }
+
+                //Establecer todas las relaciones
+                relacionesNodos(driver, pelicula, director, actor, genero);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void crearNodosPelicula(Driver driver, String persona){
+        String query = "CREATE (user:Pelicula {name: '" + persona +"'})";
+        try (Session session = driver.session()){
             session.writeTransaction(tx -> {
-                tx.run("CREATE (user:Person {name:'Adriana'})");
-                tx.run("CREATE (user:Person {name:'Bryan'})");
-                tx.run("CREATE (user:Person {name: 'Diego'})");
-                tx.run("CREATE (user:Serie {name:'Suits'})");
-                tx.run("CREATE (user:Serie {name:'Breaking Bad'})");
-                tx.run("CREATE (user:Serie {name:'Vecinos'})");
-                tx.run("CREATE (user:Genero {name:'Comedia'})");
-                tx.run("CREATE (user:Genero {name:'Accion'})");
-                tx.run("CREATE (user:Genero {name:'Drama'})");
+                tx.run(query);
                 return null;
             });
+        }
+    }
 
+    public static void crearNodosDirector(Driver driver, String persona){
+        String query = "CREATE (user:Director {name: '" + persona +"'})";
+        try (Session session = driver.session()){
+            session.writeTransaction(tx -> {
+                tx.run(query);
+                return null;
+            });
+        }
+    }
+
+    public static void crearNodosActor(Driver driver, String persona){
+        String query = "CREATE (user:Actor {name: '" + persona +"'})";
+        try (Session session = driver.session()){
+            session.writeTransaction(tx -> {
+                tx.run(query);
+                return null;
+            });
+        }
+    }
+
+    public static void crearNodosGenero(Driver driver, String persona){
+        String query = "CREATE (user:Genero {name: '" + persona +"'})";
+        try (Session session = driver.session()){
+            session.writeTransaction(tx -> {
+                tx.run(query);
+                return null;
+            });
+        } finally {
+            driver.close();
+        }
+    }
+
+    public static void relacionesNodos(Driver driver, String pelicula, String director, String actor, String genero){
+        try(Session session =driver.session()){
+            //Base para crear el match
             Result result = session.run("MATCH (n) RETURN n.name AS name");
             while (result.hasNext()) {
                 Record record = result.next();
                 String name = record.get("name").asString();
                 System.out.println("Name: " + name);
             }
+            //Relacion pelicula-director
+            String query = "MATCH (p:Pelicula {name:\"" + pelicula +"\"}), (d:Director {name:\"" + director + "\"})" +
+            "MERGE (p)-[e:DIRECTED]-(d)"+
+            "RETURN p.name, e, d.name";
+            result = session.run(query);
 
-            result = session.run("""
-                    MATCH (p:Person {name:"Adriana"}), (a:Serie {name:"Suits"})
-                    MERGE (p)-[e:WATCH]-(a)
-                    RETURN p.name, e, a.name""");
+            //Relacion pelicula-actor
+            String query1 = "MATCH (p:Pelicula {name:\"" + pelicula +"\"}), (a:Actor {name:\"" + actor + "\"})" +
+            "MERGE (p)-[e2:ACTED]-(a)"+
+            "RETURN p.name, e2, a.name";
+            result = session.run(query1);
 
-            result = session.run("""
-                    MATCH (a:Serie {name: "Suits"}), (g:Genero {name: "Drama"})
-                    MERGE (a)-[e2:GENERO]-(g)
-                    RETURN a.name, e2, g.name""");
-            
-            result = session.run("""
-                    MATCH (p:Person {name:"Bryan"}), (a:Serie {name:"Breaking Bad"})
-                    MERGE (p)-[e3:WATCH]-(a)
-                    RETURN p.name, e3, a.name""");
-
-            result = session.run("""
-                    MATCH (a:Serie {name: "Breaking Bad"}), (g:Genero {name: "Accion"})
-                    MERGE (a)-[e4:GENERO]-(g)
-                    RETURN a.name, e4, g.name""");
-                    
-            result = session.run("""
-                    MATCH (p:Person {name:"Diego"}), (a:Serie {name:"Vecinos"})
-                    MERGE (p)-[e5:WATCH]-(a)
-                    RETURN p.name, e5, a.name""");
-
-            result = session.run("""
-                MATCH (a:Serie {name: "Vecinos"}), (g:Genero {name: "Comedia"})
-                MERGE (a)-[e6:GENERO]-(g)
-                RETURN a.name, e6, g.name""");
-
-            while (result.hasNext()) {
-                Record record = result.next();
-                System.out.println(record);
-            }
-
-            /*
-
-
-            // Remove nodes to re-start.
-
-            session.writeTransaction(tx -> {
-                tx.run("MATCH (p1:Person {name:'Juan'})-[r:WATCH]-(a2:Serie {name:'Pasion de Gavilanes'}) DELETE r");
-                tx.run("MATCH (p:Person) DELETE p");
-                tx.run("MATCH (p:Serie) DELETE p");
-                tx.run("MATCH (p:Genero) DELETE p");
-                return null;
-            });
-
-             */
-
-            result = session.run("MATCH (n) RETURN n.name AS name");
-            while (result.hasNext()) {
-                Record record = result.next();
-                String name = record.get("name").asString();
-                System.out.println("Name: " + name);
-            }
-
- 
+            //Relacion pelicula-genero
+            String query2 = "MATCH (p:Pelicula {name:\"" + pelicula +"\"}), (g:Genre {name:\"" + genero + "\"})" +
+            "MERGE (p)-[e3:GENRE]-(g)"+
+            "RETURN p.name, e3, g.name";
+            result = session.run(query2);
 
         } finally {
             driver.close();
