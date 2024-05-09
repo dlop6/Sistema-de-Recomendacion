c99ff2f54fa384056a1771119a69fde7ae8b4dbf
Diego <lop23747@uvg.edu.gt>
2024-05-08 18:55:30 -0600

    menu interactivo

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -6,11 +6,25 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Nombre de archivo
-        String csvFile = "movies.csv";
+        System.out.println("Elegir opción:");
+        System.out.println("1. Crear nodos y relaciones");
+        System.out.println("2. Borrar nodos y relaciones");
+        Scanner scanner = new Scanner(System.in);
+        int opcion = scanner.nextInt();
+        if(opcion == 1){
+            createNodesandConnections();
+        } else if(opcion == 2){
+            DeleteAllNodesAndRelationships.main(args);
+        }
+
+    }
+
+    public static void createNodesandConnections(){
+        String csvFile = "pruebasneo4j\\movies.csv";
         String line;
         Driver driver = GraphDatabase.driver(
                 "neo4j+s://9c5d5c00.databases.neo4j.io", //"bolt://localhost:7687",
@@ -48,49 +62,58 @@ public class Main {
                 }
 
                 //Obtener nombre Director, y validar si se agrega
-                String director = data[1];
+                String director = "";
+                if (data.length > 1) {
+                    director = data[1];
+                }
                 for(String nodo:nodosAgregados){
                     if(director.equals(nodo)){
                         directorEncontrado = true;
                     }
-                } if(!directorEncontrado){
+                } if(!directorEncontrado && !director.isEmpty()){
                     crearNodosDirector(driver, director);
                     nodosAgregados.add(director);
                 }
 
                 //Obtener actor principal, y validar si se agrega
-                String actor = data[2];
+                String actor = "";
+                if (data.length > 2) {
+                    actor = data[2];
+                }
                 for(String nodo:nodosAgregados){
                     if(actor.equals(nodo)){
                         actorEncontrado = true;
                     }
-                } if(!actorEncontrado){
+                } if(!actorEncontrado && !actor.isEmpty()){
                     crearNodosActor(driver, actor);
                     nodosAgregados.add(actor);
                 }
 
                 //Obtener Género, y validar si se agrega
-                String genero = data[3];
+                String genero = "";
+                if (data.length > 3) {
+                    genero = data[3];
+                }
                 for(String nodo: nodosAgregados){
                     if(genero.equals(nodo)){
                         generoEncontrado = true;
                     }
-                } if(!generoEncontrado){
+                } if(!generoEncontrado && !genero.isEmpty()){
                     crearNodosGenero(driver, genero);
                     nodosAgregados.add(genero);
                 }
 
                 //Establecer todas las relaciones
                 relacionesNodos(driver, pelicula, director, actor, genero);
-
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        //Se cierra el driver
     }
 
-    public static void crearNodosPelicula(Driver driver, String persona){
-        String query = "CREATE (user:Pelicula {name: '" + persona +"'})";
+    public static void crearNodosPelicula(Driver driver, String pelicula){
+        String query = "CREATE (user:Pelicula {name: '" + pelicula.replaceAll("'", "\\\\'") +"'})";
         try (Session session = driver.session()){
             session.writeTransaction(tx -> {
                 tx.run(query);
@@ -99,8 +122,8 @@ public class Main {
         }
     }
 
-    public static void crearNodosDirector(Driver driver, String persona){
-        String query = "CREATE (user:Director {name: '" + persona +"'})";
+    public static void crearNodosDirector(Driver driver, String director){
+        String query = "CREATE (user:Director {name: '" + director.replaceAll("'", "\\\\'") +"'})";
         try (Session session = driver.session()){
             session.writeTransaction(tx -> {
                 tx.run(query);
@@ -109,8 +132,8 @@ public class Main {
         }
     }
 
-    public static void crearNodosActor(Driver driver, String persona){
-        String query = "CREATE (user:Actor {name: '" + persona +"'})";
+    public static void crearNodosActor(Driver driver, String actor){
+        String query = "CREATE (user:Actor {name: '" + actor.replaceAll("'", "\\\\'") +"'})";
         try (Session session = driver.session()){
             session.writeTransaction(tx -> {
                 tx.run(query);
@@ -119,16 +142,14 @@ public class Main {
         }
     }
 
-    public static void crearNodosGenero(Driver driver, String persona){
-        String query = "CREATE (user:Genero {name: '" + persona +"'})";
+    public static void crearNodosGenero(Driver driver, String genero){
+        String query = "CREATE (user:Genero {name: '" + genero.replaceAll("'", "\\\\'") +"'})";
         try (Session session = driver.session()){
             session.writeTransaction(tx -> {
                 tx.run(query);
                 return null;
             });
-        } finally {
-            driver.close();
-        }
+        } 
     }
 
     public static void relacionesNodos(Driver driver, String pelicula, String director, String actor, String genero){
@@ -158,8 +179,25 @@ public class Main {
             "RETURN p.name, e3, g.name";
             result = session.run(query2);
 
-        } finally {
-            driver.close();
         }
     }
+
+    public class DeleteAllNodesAndRelationships {
+        public static void main(String[] args) {
+            Driver driver = GraphDatabase.driver("neo4j+s://9c5d5c00.databases.neo4j.io", AuthTokens.basic("neo4j", "xgq5hyZRycODNp5U7cQuTg2GwIWNePhoKn4ZOLZoE30"));
+    
+            try (Session session = driver.session()) {
+                session.writeTransaction(tx -> {
+                    tx.run("MATCH (n) DETACH DELETE n");
+                    return null;
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                driver.close();
+            }
+        }
+    }
+
+
 }
